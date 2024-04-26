58c3129e792f75f53edc166297a108a7040a8edd
EsrefDesign <148644714+esrefdesign@users.noreply.github.com>
2024-04-27 00:26:26 +0300

    start.dart
    Start Page Design Finish

--- order_go/lib/pages/start.dart
+++ order_go/lib/pages/start.dart
@@ -7,8 +7,8 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:order_go/theme/color.dart';
 
-class Start_page extends StatelessWidget {
-  const Start_page({super.key});
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +21,8 @@ class Start_page extends StatelessWidget {
         actions: <Widget>[
         IconButton(
           
-          icon: Icon(Icons.person),
-          padding: EdgeInsets.only(right: 30),
+          icon:const  Icon(Icons.person),
+          padding: const EdgeInsets.only(right: 30),
           iconSize: 40,
           color: bg,
           onPressed: () {
@@ -33,8 +33,8 @@ class Start_page extends StatelessWidget {
 
         //Menu Button
         leading: IconButton(
-          padding: EdgeInsets.only(left: 30,bottom: 10),
-          icon: Icon(Icons.menu),
+          padding: const EdgeInsets.only(left: 30,bottom: 10),
+          icon: const Icon(Icons.menu),
           iconSize: 40,
           color: bg,
           onPressed: () {
@@ -42,7 +42,7 @@ class Start_page extends StatelessWidget {
           },
         ),
         toolbarHeight: 100,
-        shape:RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
         backgroundColor:mainColor,
         flexibleSpace: FlexibleSpaceBar(
           background: 
@@ -53,10 +53,57 @@ class Start_page extends StatelessWidget {
           
         ),
     
-        
-        
       ),
       
+      extendBodyBehindAppBar: true,
+
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          
+           Stack(
+              children: [
+                SvgPicture.asset('assets/backgorund/background.svg',alignment: Alignment.topCenter,height:930,),
+                Padding(
+                  padding: const EdgeInsets.only(top: 270.0,left: 70),
+                  child: SvgPicture.asset('assets/pictres/undraw_off_road_re_leme.svg',width: 300,),
+                ),
+              ],
+             ),
+           
+             Align(
+              alignment: Alignment.bottomCenter,
+               child: Text.rich(
+                  TextSpan( 
+                    text: 'Designed by ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: mainColor,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Poppins"
+                      
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Bug Zappers",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: mainColor,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Poppins"
+                        )
+                      )
+                    ]
+                  )
+                  
+                ),
+             ),
+            
+          
+        ],
+      )
+      
+
       
 
     );
