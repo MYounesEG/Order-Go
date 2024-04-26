9fbf4d05e0be95327081281393dff2fa786158dc
esrefdesign <esrefdesign@gmail.com>
2024-04-26 21:07:38 +0300

    Update start style

--- order_go/lib/pages/start.dart
+++ order_go/lib/pages/start.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -12,13 +13,51 @@ class Start_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.blue,
-          height: 200,
-          width: 200,
+      backgroundColor: bg,
+    
+      appBar: AppBar(
+        
+        //Profile
+        actions: <Widget>[
+        IconButton(
+          
+          icon: Icon(Icons.person),
+          padding: EdgeInsets.only(right: 30),
+          iconSize: 40,
+          color: bg,
+          onPressed: () {
+            // Arama işlemini tetikle
+          },
+        )
+        ],
+
+        //Menu Button
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 30,bottom: 10),
+          icon: Icon(Icons.menu),
+          iconSize: 40,
+          color: bg,
+          onPressed: () {
+            
+          },
         ),
+        toolbarHeight: 100,
+        shape:RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        backgroundColor:mainColor,
+        flexibleSpace: FlexibleSpaceBar(
+          background: 
+          Padding(
+            padding: const EdgeInsets.only(top: 80.0,bottom: 30,right: 33,left: 33),
+            child: SvgPicture.asset('assets/order_go.svg'),
+          ),
+          
+        ),
+    
+        
+        
       ),
+      
+      
 
     );
   }
