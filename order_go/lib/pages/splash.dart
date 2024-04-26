32a556ff2796a6ca632b4349630ebcc21c93e207
esrefdesign <esrefdesign@gmail.com>
2024-04-26 17:23:58 +0300

    pictures assets

---
+++ order_go/lib/pages/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SpalashScreen extends StatefulWidget {
+  const SpalashScreen({super.key});
+
+  @override
+  State<SpalashScreen> createState() => _SpalashScreenState();
+}
+
+class _SpalashScreenState extends State<SpalashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
