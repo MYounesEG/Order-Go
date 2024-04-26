711869d8d291950e69d7d57380057bd27e697682
esrefdesign <esrefdesign@gmail.com>
2024-04-26 18:40:51 +0300

    fonts uploaded

--- order_go/lib/pages/splash.dart
+++ order_go/lib/pages/splash.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:order_go/theme/color.dart';
 
-class SpalashScreen extends StatefulWidget {
-  const SpalashScreen({super.key});
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
-  @override
-  State<SpalashScreen> createState() => _SpalashScreenState();
-}
-
-class _SpalashScreenState extends State<SpalashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        SvgPicture.asset(
-          'order_go_wb.svg',
+      backgroundColor: mainColor,
+      
+      //Splash Screen Elements
+
+      body: ListView(children: [
+        //Logo
+
+        Center(
+          child: Padding(
+            
+            
+            padding: const EdgeInsets.all(80.0),
+            
+            child: SvgPicture.asset(
+              
+              'assets/order_go_wb.svg',
+              height: 670,
+            
+            ),
+
+          //text
+
+          
+            
+          ),
         )
       ],),
       
-        
+      
     );
   }
 }
--- order_go/lib/theme/color.dart
+++ order_go/lib/theme/color.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color bg = const Color.fromARGB(255, 27, 27, 27);
-Color mainColor = const Color.fromARGB(255,51,182,1);
+Color mainColor = Color.fromARGB(255, 51, 255, 182);
 Color container = const Color.fromARGB(49, 49, 49, 1);
